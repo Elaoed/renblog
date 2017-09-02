6db661d2f660db42fd854d1f657a4610d1fb3b1a
Delphi <renxiaopeng@newdun.com>
2017-09-02 14:16:48 +0800

    fix css

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # encoding=utf-8
-u"""Made by delphi for himself's blog"""
+"""Made by delphi for himself's blog"""
 
 import os
-import binascii
-# from lxml import etree
+# import binascii
+from lxml import etree
 
 from flask import Flask
 from flask import render_template
@@ -16,58 +16,48 @@ ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 @app.route("/")
 def index():
 
-    articles = os.listdir(os.path.dirname(os.path.realpath(__file__)) + "/articles")
+    excludes = [".DS_Store"]
+    articles = os.listdir(os.path.dirname(os.path.realpath(__file__)) + "/templates/articles")
+    articles = [a for a in articles if a not in excludes]
     data = {
         'articles': articles,
-        'create_data': "",
-        'tags': ""
     }
     return render_template("home.html", data=data)
 
 
-# class ErrorHandler(BaseHandler):
-#     """"""
+@app.route("/404")
+def get(self):
+    """404 page"""
+    return render_template("404.html")
 
-#     def get(self):
-#         return self.finish("404")
+@app.route("/about")
+def about():
+    """ Resume of myself"""
+    return render_template("about.html")
 
+@app.route("/articles/<name>")
+def article(name=""):
 
-# class AboutHandler(BaseHandler):
-#     """Probably it's a resume of myself"""
+    # file_path = ROOT_PATH + "/articles/" + name
+    # if not os.path.exists(file_path):
+    #     return "404"
+    file_path = "articles/" + "Phrase.html"
 
-#     def get(self):
-#         return self.render("about.html")
-
-# class SeoHandler(BaseHandler):
-#     """Probably it's a resume of myself"""
-
-#     def get(self):
-#         return self.render("seo.html")
-
-
-# class ArticleHandler(BaseHandler):
-#     """Article page"""
-
-#     def get(self):
-#         article_name = self.get_argument("name", "")
-#         if not article_name:
-#             self.redirect('/404')
-#         file_path = ROOT_PATH + "/articles/" + article_name + ".html"
-#         # with open(file_path, 'r') as f:
-#         #     article = f.read()
-#         # selector = etree.HTML(article)
-#         # note = selector.xpath("/html/body/div[@class=\"note-wrapper\"]")
-#         # title = selector.xpath("/html/body/div[@class=\"note-wrapper\"]/h1/text()")
-#         # title = title[0].encode('utf-8')
-#         # content = note[0].xpath('string(.)').encode('utf-8')
-#         # data = {
-#         #     'title': title,
-#         #     'content': content
-#         # }
-#         return self.render(file_path)
+    # with open(file_path, 'r') as f:
+    #     article = f.read()
+    # selector = etree.HTML(article)
+    # note = selector.xpath("/html/body/div[@class=\"note-wrapper\"]")
+    # title = selector.xpath("/html/body/div[@class=\"note-wrapper\"]/h1/text()")
+    # title = title[0].encode('utf-8')
+    # content = note[0].xpath('string(.)').encode('utf-8')
+    # data = {
+    #     'title': title,
+    #     'content': content
+    # }
+    return render_template(file_path)
 
 
 if __name__ == "__main__":
 
     print("Running on the port 8080......")
-    app.run(host='0.0.0.0', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=8081, debug=False)
